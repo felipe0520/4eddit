a19d9982839cc641f231ae7e2ef1c63d37f2d6a9
Felipe-Ferraz <felipeefrz1991@gmail.com>
2020-06-30 14:51:50 -0300

    manutençao em filtro

---
+++ build/precache-manifest.a9b4af97f9447ba939baf132ad364237.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2621a10ce1683f4460fc738bb8cf61f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e87dba934399720611a3",
+    "url": "/static/js/2.a505f741.chunk.js"
+  },
+  {
+    "revision": "f129a25a95b5779003d2444137eb2900",
+    "url": "/static/js/2.a505f741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72465a5691288bbc21c1",
+    "url": "/static/js/main.860fdd68.chunk.js"
+  },
+  {
+    "revision": "b75208a6ae0b86d97c68",
+    "url": "/static/js/runtime-main.87a68ae9.js"
+  }
+]);
--- src/reducers/postList.js
+++ src/reducers/postList.js
@@ -1,33 +1,34 @@
 const initialSate = {
-    posts: [],
-    postDetail: {},
-    filter: undefined
-}
+  posts: [],
+  postDetail: {},
+  filter: undefined,
+};
 
 const ReducerPost = (state = initialSate, action) => {
-    switch (action.type) {
-        case "GET_POSTS":
-            return { ...state, posts: action.payload.data }
+  switch (action.type) {
+    case "GET_POSTS":
+      return { ...state, posts: action.payload.data };
 
-        case "POST_DETAILS":
-            return { ...state, postDetail: action.payload.data }
+    case "POST_DETAILS":
+      return { ...state, postDetail: action.payload.data };
 
-        case "FILTER":
-            const valueFilter = action.payload.valueFilter;
-            if (valueFilter) {
-                const postsFilter = state.posts.filter(post => {
-                    return (post.text.toLowerCase().indexOf(valueFilter.toLowerCase()) > -1)
-                })
-                return { ...state, filter: postsFilter }
-            } else {
-                return { ...state, filter: undefined }
-            }
+    case "FILTER":
+      const valueFilter = action.payload.valueFilter;
+      if (valueFilter) {
+        const postsFilter = state.posts.filter((post) => {
+          return (
+            post.text &&
+            post.text.toLowerCase().indexOf(valueFilter.toLowerCase()) > -1
+          );
+        });
+        return { ...state, filter: postsFilter };
+      } else {
+        return { ...state, filter: undefined };
+      }
 
-
-        default:
-            return state
-
-    }
-}
+    default:
+      return state;
+  }
+};
 
 export default ReducerPost;
